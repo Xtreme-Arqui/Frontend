9d4195956cde5577b72367b84b7024b9b1552c00
Sheiger <emersonquispe2808@gmail.com>
2024-10-28 18:19:30 -0500

    add component routes

---
+++ src/app/adventurer/pages/routes/routes.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Route } from '../../../agency/models/route.model';
+import { RouteService } from '../../../agency/services/route.service';
+
+@Component({
+  selector: 'app-routes',
+  templateUrl: './routes.component.html',
+  styleUrl: './routes.component.css'
+})
+export class RoutesComponent implements OnInit{
+  routes: Route [] = [];
+  
+
+  constructor(private RouteService: RouteService) {}
+
+  ngOnInit(): void {
+    this.getRoutes();
+  }
+
+  getRoutes(): void {
+    this.RouteService.getRoutes().subscribe(
+      (data) => {
+        this.routes = data;
+        console.log(this.routes);
+      }
+    )
+  }
+}
