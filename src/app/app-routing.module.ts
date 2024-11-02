2f22a154a9463eb148efa529b137142c5f56387d
Sheiger <emersonquispe2808@gmail.com>
2024-11-02 09:08:47 -0500

    app routing module update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderContentComponent} from "./web/components/header-content/header-content.component";
-import { PanelComponent} from "./web/components/panel/panel.component";
-import { PublicationsComponent} from "./agency/pages/publications/publications.component";
-import { ProfileAgencyComponent} from "./agency/pages/profile-agency/profile-agency.component";
-import { MonitoringComponent } from './agency/pages/monitoring/monitoring.component';
-import { HomeComponent } from './agency/pages/home/home.component';
-import { HomeTouristComponent } from './adventurer/pages/home-tourist/home-tourist.component';
-import { RoutesComponent } from './adventurer/pages/routes/routes.component';
-import { FilterComponent } from './adventurer/pages/filter/filter.component';
-import { PackageComponent } from './adventurer/pages/package/package.component';
-import { AgencyComponent } from './adventurer/pages/agency/agency.component';
-import { ProfileComponent } from './adventurer/pages/profile/profile.component';
-import { DataAdventurerComponent } from './adventurer/pages/data-adventurer/data-adventurer.component';
-import { AccountAdventurerComponent } from './adventurer/pages/account-adventurer/account-adventurer.component';
+import { AuthComponent } from './access/pages/auth/auth.component';
+import { PageNotFoundComponent } from '../shared/components/page-not-found/page-not-found.component';
+import { AccessDeniedComponent } from '../shared/components/access-denied/access-denied.component';
+import { authGuard } from './access/guards/auth.guard';
+import { SelectTypeComponent } from './access/pages/select-type/select-type.component';
+import { RegisterAgencyComponent } from './access/pages/register-agency/register-agency.component';
+import { RegisterTouristComponent } from './access/pages/register-tourist/register-tourist.component';
+
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home/routes', pathMatch: 'full'},
-  { path: 'home', component: HomeTouristComponent, children:[
-    { path: 'routes', component: RoutesComponent},
-    { path: 'filters', component: FilterComponent},
-    { path: 'package/:id', component: PackageComponent},
-    { path: 'agency/:id', component: AgencyComponent},
-  ]},
-  { path: 'profile', component: ProfileComponent, children:[
-    { path: 'data', component: DataAdventurerComponent},
-    { path: 'account', component: AccountAdventurerComponent}
-  ]}
-  /*{ path: 'home', component: HomeComponent, children: [
-    {path: 'services', component: PublicationsComponent},
-    {path: 'monitoring', component: MonitoringComponent},
-    {path: 'profile', component: ProfileAgencyComponent},
-    ] 
-  },*/
+  { path: '', redirectTo: '/access', pathMatch: 'full'},
+  { path: 'access', component: AuthComponent, pathMatch: 'full'},
+  { path: 'select-type', component: SelectTypeComponent, pathMatch: 'full'},
+  { path: 'register-agency', component: RegisterAgencyComponent, pathMatch: 'full'},
+  { path: 'register-tourist', component: RegisterTouristComponent, pathMatch: 'full'},
+  { 
+    path: 'home', 
+    loadChildren: () => import('./adventurer/adventurer-module/home.module').then(m => m.HomeModule), 
+    canActivate: [authGuard], 
+    data: { expectedUserType: 'adventurer' } 
+  },
+  { 
+    path: 'profile', 
+    loadChildren: () => import('./adventurer/adventurer-module/profile.module').then(m => m.ProfileModule), 
+    canActivate: [authGuard], 
+    data: { expectedUserType: 'adventurer' } 
+  },
+
+  // Carga diferida (Lazy Loading) para el módulo de Agencias
+  { 
+    path: 'home', 
+    loadChildren: () => import('./agency/agency-module/agency.module').then(m => m.AgencyModule), 
+    canActivate: [authGuard], 
+    data: { expectedUserType: 'agency' } 
+  },
+  { path: 'access-denied', component: AccessDeniedComponent },
+  { path: '**', component: PageNotFoundComponent}
+  
 
 
 ];
