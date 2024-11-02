337f84a4345da12dd94afd1969c39c59012aa947
Sheiger <emersonquispe2808@gmail.com>
2024-11-02 08:56:17 -0500

    update of the component to see all routes

--- src/app/adventurer/pages/profile/profile.component.ts
+++ src/app/adventurer/pages/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../../../access/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -20,7 +21,8 @@ export class ProfileComponent implements OnInit{
   constructor(
     //private authService: AuthService,
     private breakpointObserver: BreakpointObserver,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -28,5 +30,9 @@ export class ProfileComponent implements OnInit{
 
   isSelected(path: string): boolean {
     return this.router.url === path;
+  }
+
+  onLogOut(): void {
+    this.authService.logOut();
   }
 }
